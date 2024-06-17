f0acc33ead5a764181a0082c2d76abc4d0de0243
deby <escuderoflores1993@gmail.com>
2024-06-17 08:47:30 +0200

    gitignore hecho

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`)
+}) 
+
+const express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+    res.send('Hello Express')
+});
+app.listen(process.env.PORT || 3000)
